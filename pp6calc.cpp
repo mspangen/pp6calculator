635bc14e99da646d8b0da8088ed0e2c5df8398d7
Martin Spangenberg <m.spangenberg@warwick.ac.uk>
2013-02-18 16:22:21 +0000

    Add template functions for solving quadratic equation etc.

--- pp6calc.cpp
+++ pp6calc.cpp
@@ -1,25 +1,52 @@
 #include <iostream>
 #include <climits>
+#include <cmath>
+#include <vector>
 
-void input_error()
-{
-	std::cin.clear();
-	std::cin.ignore(INT_MAX, '\n');
-}
+#include "pp6calc.h" // Include header file to forward declare functions
 
 int main()
 {
 
 	double a;
 	double b;
-	double c;
 	std::string operation;
+
+	std::vector<std::string> valid_operations; // List of valid operations
+	valid_operations.push_back("q");
+	valid_operations.push_back("a");
+	valid_operations.push_back("s");
+	valid_operations.push_back("m");
+	valid_operations.push_back("d");
+	valid_operations.push_back("i");
+	valid_operations.push_back("p");
+	valid_operations.push_back("v3");
+	valid_operations.push_back("v4");
+	valid_operations.push_back("im");
+
+	bool valid; // Boolean to check if operation choice is valid
+
 
 	while (true)
 	{
 
+		valid = false;
+
 		std::cout << "Choose operation [add (a), subtract (s), multiply (m), divide (d)]:\n";
 		std::cin >> operation;
+
+		for (unsigned int i=0; i<valid_operations.size(); ++i)
+		{
+			if (valid_operations.at(i) == operation) valid = true;
+		}
+
+		// Check if user entered quit command or if operation does not exist
+		if (operation == "q") return 0;
+		else if (!valid) {
+			std::cout << "Error in operation choice!\n";
+			input_error();
+			continue;
+		}
 
 		std::cout << "Please enter number A:\n";
 		std::cin >> a;
@@ -34,29 +61,103 @@ int main()
 		}
 
 		if (operation == "a") {
-			std::cout << "A+B = " << a+b << std::endl;
+			std::cout << "A+B = " << Add(a,b) << std::endl;
 		} else if (operation == "s") {
-			std::cout << " A-B = " << a-b << std::endl;
+			std::cout << " A-B = " << Subtract(a,b) << std::endl;
 		} else if (operation == "m") {
-			std::cout << "A*B = " << a*b << std::endl;
+			std::cout << "A*B = " << Multiply(a,b) << std::endl;
 		} else if (operation == "d") { 
 			if (b == 0) {
 				std::cout << "Division by zero!\n";
 				input_error();
 				continue;
 			}
-			std::cout << "A/B = " << a/b << std::endl;
+			std::cout << "A/B = " << Divide(a,b) << std::endl;
 		} else if (operation == "q") {
 			break;
 		}
-		else {
-			std::cout << "Error in operation choice!\n";
-			input_error();
-			continue;
-		}
+
 
 	}
 
 	return 0;
 
 }
+
+
+
+
+//==================================
+// Functions for calculator
+//==================================
+
+//Function to call whenever an input exception is found
+void input_error()
+{
+	std::cin.clear();
+	std::cin.ignore(INT_MAX, '\n');
+}
+
+//-------------------
+// Math functions
+//-------------------
+
+double Add (double a, double b)
+{
+	return a+b;
+}
+
+double Subtract (double a, double b)
+{
+	return a-b;
+}
+
+double Multiply (double a, double b)
+{
+	return a*b;
+}
+
+double Divide (double a, double b)
+{
+	return a/b;
+}
+
+// Calculate the intercept of a line on the x-axis
+double Intercept (double a, double b) //a*x+b
+{
+	
+}
+
+// Solve quadratic equation
+double Poly2Solve (double a, double b, double c)
+{
+
+}
+
+// Calculate length of 3-vector
+double Vector3Length (double v1, double v2, double v3)
+{
+
+}
+
+// Calculate length of 4-vector
+double Vector4Length (double v1, double v2, double v3)
+{
+
+}
+
+// Calculate invariant mass of particle
+double InvMass (double m1, double m2, double angle)
+{
+
+}
+
+
+
+
+//==================================
+// Manual
+//==================================
+
+//Write manual text to be output when running program with --man
+
---
+++ pp6calc.h
@@ -0,0 +1,26 @@
+#ifndef _PP6CALC_H_
+#define _PP6CALC_H_
+
+void input_error();
+double Add (double, double);
+double Subtract (double, double);
+double Multiply (double, double);
+double Divide (double, double);
+double Intercept (double, double);
+double Poly2Solve (double, double, double);
+double Vector3Length (double, double, double);
+double Vector4Length (double, double, double, double);
+double InvMass (double, double, double);
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
