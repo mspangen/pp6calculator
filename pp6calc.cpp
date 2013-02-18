f178ce65ecb24b34fd16692ee320a3bc5a33a367
Martin Spangenberg <m.spangenberg@warwick.ac.uk>
2013-02-18 15:18:47 +0000

    Add basic number operations

--- pp6calc.cpp
+++ pp6calc.cpp
@@ -6,15 +6,35 @@ int main()
 	double a;
 	double b;
 	double c;
+	std::string operation;
+
+	std::cout << "Choose operation [add (a), subtract (s), multiply (m), divide (d)]:\n";
+	std::cin >> operation;
 
 	std::cout << "Please enter number A:\n";
 	std::cin >> a;
 	std::cout << "Please enter number B:\n";	
 	std::cin >> b;
 
-	c = a*b;
+	if (!std::cin)
+	{
+		std::cout << "Error in input! \n";
+		return 0;
+	}
 
-	std::cout << "A*B = " << c << std::endl;
+	if (operation == "a") {
+		std::cout << "A+B = " << a+b << std::endl;
+	} else if (operation == "s") {
+		std::cout << " A-B = " << a-b << std::endl;
+	} else if (operation == "m") {
+		std::cout << "A*B = " << a*b << std::endl;
+	} else if (operation == "d") { 
+		if (b == 0) {
+			std::cout << "Division by zero!\n";
+			return 0;
+		}
+		std::cout << "A/B = " << a/b << std::endl;
+	}
 
 	return 0;
 
