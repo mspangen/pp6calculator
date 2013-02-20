82992f4f9009de7f1cf5120d81e1af9fa0e57ba7
Martin Spangenberg <m.spangenberg@warwick.ac.uk>
2013-02-20 18:04:09 +0000

    Change input method to comma separated string. Add functions for calculating 3-vector and 4-vector sizes.

--- pp6calc.cpp
+++ pp6calc.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <sstream>
 #include <climits>
 #include <cmath>
 #include <string>
 #include <vector>
 #include <set>
+#include <algorithm>
 
 #include "pp6calc.h" // Include header file to forward declare functions
 
@@ -15,19 +17,12 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 
-	// Variables used by calculator
-	double a;
-	double b;
-	double c;
-
-	std::string operation; // Temporary variable to store operation chosen by user
+	std::string operation; // Variable to store operation chosen by user
 
 	//Define set of available operations
 	const int Noperations = 10;
 	std::string valid_operations[Noperations] = {"q","a","s","m","d","i","p","v3","v4","im"};
-	std::set<std::string> valid_operations_set; // Set of valid operations
-	for (int i=0; i<Noperations; ++i) valid_operations_set.insert(valid_operations[i]);
-
+	std::set<std::string> valid_operations_set (valid_operations,valid_operations+Noperations); // Set of valid operations
 
 	while (true) // Loop over calculator menu until user quits with "q"
 	{
@@ -42,46 +37,41 @@ int main(int argc, char* argv[])
 			return 0;
 		}
 
-		// Prompt user for numbers to process depending on operation chosen
-		std::cout << "Enter number A:\n";
-		std::cin >> a;
-		std::cout << "Enter number B:\n";	
-		std::cin >> b;
-		if (operation == "p") {
-			std::cout << "Enter number C:\n";
-			std::cin >> c;
-		}
-
-		if (!std::cin) { // Check for errors in input
-			std::cout << "Error in input! \n";
-			input_error();
+		vars = get_user_input(operation); // Prompt user for numbers to process depending on operation chosen
+		if (vars.size() == 0) { // If size is zero then there are errors in input
+			std::cout << "Error in input!\n";
 			continue;
 		}
 
 		// Calculate result depending on the operation chosen
 		if (operation == "a") {
-			std::cout << "A+B = " << Add(a,b) << std::endl;
+			std::cout << vars.at(0) << " + " << vars.at(1) << " = " << Add(vars.at(0),vars.at(1)) << std::endl;
 		}
+		
 		else if (operation == "s") {
-			std::cout << " A-B = " << Subtract(a,b) << std::endl;
+			std::cout << vars.at(0) << " - " << vars.at(1) << " = " << Subtract(vars.at(0),vars.at(1)) << std::endl;
 		}
+		
 		else if (operation == "m") {
-			std::cout << "A*B = " << Multiply(a,b) << std::endl;
+			std::cout << vars.at(0) << " * " << vars.at(1) << " = " << Multiply(vars.at(0),vars.at(1)) << std::endl;
 		}
-		else if (operation == "d") { 
-			if (b == 0) {
+		
+		else if (operation == "d") {
+			if (vars.at(1) == 0) {
 				std::cout << "Division by zero!\n";
 				input_error();
 				continue;
 			}
-			std::cout << "A/B = " << Divide(a,b) << std::endl;
+			std::cout << vars.at(0) << " / " << vars.at(1) << " = " << Divide(vars.at(0),vars.at(1)) << std::endl;
 		}
+		
 		else if (operation == "i") {
-			std::cout << "Line intercepts x-axis at x = " << Intercept(a,b) << std::endl;
+			std::cout << "Line " << vars.at(0) << "*x+" << vars.at(1) << " intercepts x-axis at x = " << Intercept(vars.at(0),vars.at(1)) << std::endl;
 		}
+		
 		else if (operation == "p") {
-			std::vector<double> solutions = Poly2Solve(a,b,c);
-			std::cout << "Solution to 2nd degree polynomial:\n";
+			std::vector<double> solutions = Poly2Solve(vars.at(0),vars.at(1),vars.at(2));
+			std::cout << "Solution to 2nd degree polynomium " << vars.at(0) << "*x^2+" << vars.at(1) << "*x+" << vars.at(2) << "=0:\n";
 			for (unsigned int i=0; i<solutions.size(); ++i)
 			{
 				std::cout << "x" << i << " = " << solutions.at(i) << std::endl;
@@ -90,9 +80,32 @@ int main(int argc, char* argv[])
 				std::cout << "None" << std::endl;
 			}
 		}
+		
+		else if (operation == "v3") {
+			double result = Size3Vec(vars.at(0),vars.at(1),vars.at(2));
+			std::cout << "Size of 3-vector (" << vars.at(0) << "," << vars.at(1) << "," << vars.at(2) << ") = " << result << "\n";
+		}
+
+		else if (operation == "v4") {
+			double result = Size4Vec(vars.at(0),vars.at(1),vars.at(2),vars.at(3));
+			if (result >= 0) {
+				std::cout << "Size of 4-vector (" << vars.at(0) << "," << vars.at(1) << "," << vars.at(2) << "," << vars.at(3) << ") = " << result << "\n";
+			} else {
+				std::cout << "Invalid entry. Negative square of components\n";
+			}
+		}
+
+		else if (operation == "im") {
+		//	double result = InvariantMass(vars.at(0),vars.at(1),vars.at(2),vars.at(3),...);
+		//	std::cout << "Invariant mass of particles M = " << result << std::endl;
+		}
+
 		else if (operation == "q") {
 			break;
 		}
+
+
+
 
 	}
 
@@ -107,12 +120,114 @@ int main(int argc, char* argv[])
 // Functions for calculator
 //==================================
 
-//Function to call whenever an input exception is found
+// Function to call whenever an input exception is found
 void input_error()
 {
 	std::cin.clear();
 	std::cin.ignore(INT_MAX, '\n');
 }
+
+// Function to get user input
+std::vector<double> get_user_input(std::string operation)
+{
+	std::vector<double> variables;
+	std::string prompt_text;
+	int nvars;
+
+	if (operation == "a") {
+		prompt_text = "Addition A+B. Enter as A,B:";
+		nvars = 2;
+	} else if (operation == "s") {
+		prompt_text = "Subtraction A-B. Enter as A,B:";
+		nvars = 2;
+	} else if (operation == "m") {
+		prompt_text = "Multiplication A*B. Enter as A,B:";
+		nvars = 2;
+	} else if (operation == "d") {
+		prompt_text = "Division A/B. Enter as A,B:";
+		nvars = 2;
+	} else if (operation == "i") {
+		prompt_text = "Intersect of line A*x+B with x-axis. Enter as A,B:";
+		nvars = 2;
+	} else if (operation == "p") {
+		prompt_text = "Solution to 2nd order polynomial A*x^2+B*x+C=0. Enter as A,B,C:";
+		nvars = 3;
+	} else if (operation == "v3") {
+		prompt_text = "Size of 3-vector (v1,v2,v3). Enter as v1,v2,v3:";
+		nvars = 3;
+	} else if (operation == "v4") {
+		prompt_text = "Size of 4-vector (v0,v1,v2,v3). Enter as v0,v1,v2,v3:";
+		nvars = 4;
+	} else if (operation == "im") {
+		prompt_text = "Invariant mass of particle pair. Enter as E1,E2,"; // ???
+		nvars = 4; // ???
+	}
+	
+	std::cout << prompt_text << std::endl;
+
+	std::string input;
+	std::cin >> input;
+
+	if (!std::cin) { // Check for errors in input
+		input_error();
+	}
+	else {
+		variables = interpret_input(input);
+	}
+
+	if (!(variables.size() == nvars)) {
+		variables.clear();
+	}
+
+	return variables;
+
+}
+
+// Function to interpret input string
+// This splits comma separated string into input variables
+std::vector<double> interpret_input (std::string input)
+{
+	std::vector<double> variables; // Vector of variables to output
+	size_t lastfound = -1;
+	size_t found;
+	std::string variable;
+	double doublevar;
+	std::string error_msg; // Error message if error found in variable
+
+	while (true) {
+		found = input.find(",",lastfound+1); // Position of comma in string
+		if (found != std::string::npos) { // Check if a comma was found. If not, found==std::string::npos
+			variable = input.substr(lastfound+1,found-(lastfound+1));
+		} else {
+			variable = input.substr(lastfound+1,input.length()-(lastfound+1));
+		}
+
+		lastfound = found; // Update position of last found comma
+
+		// Check if the variable is a valid number
+		if (variable.empty()) error_msg = "Empty variable!";
+		else if (variable.find_first_not_of("0123456789.-") != std::string::npos) error_msg = "Invalid characters!";
+		else if (std::count(variable.begin(), variable.end(),'.') > 1) error_msg = "Too many periods!";
+		else if (variable.find_first_of("-",1) != std::string::npos) error_msg = "Minus sign inside number!";
+
+		if (!error_msg.empty())	{ // If errors are found clear variable vector and exit loop
+			variables.clear();
+			std::cout << error_msg << std::endl;
+			break;
+		}
+
+		std::stringstream convert(variable);
+		convert >> doublevar; // Convert variable string to double
+
+		variables.push_back(doublevar);
+
+		if (found == std::string::npos) break;
+	}
+
+	return variables;
+
+}
+
 
 //-------------------
 // Math functions
@@ -223,12 +338,12 @@ void helptext()
 	std::cout << "========================\n";
 	std::cout << "How to use this program \n";
 	std::cout << "========================\n";
-	std::cout << "1) Enter the operation that you want to perform when prompted.\n";
+	std::cout << "Enter the operation that you want to perform when prompted.\n";
 	std::cout << "   Available operations:\n";
-	std::cout << "   (a) Addition a+b, (s) Subtraction a-b, (m) Multiplication a*b, (d) Division a/b,\n";
-	std::cout << "   (i) Find intercept of straight line ax+b with x-axis,\n";
-	std::cout << "   (p) Solve 2nd degree polynomial ax^2 + bx + c = 0,\n";
+	std::cout << "   (a)  Addition a+b, (s) Subtraction a-b, (m) Multiplication a*b, (d) Division a/b,\n";
+	std::cout << "   (i)  Find intercept of straight line a*x+b with x-axis,\n";
+	std::cout << "   (p)  Solve 2nd degree polynomial a*x^2 + b*x + c = 0,\n";
 	std::cout << "   (v3) Calculate size of 3-vector (a, b, c),\n";
 	std::cout << "   (v4) Calculate size of 4-vector (a, b, c, d) assuming metric (1, -1, -1, -1),\n";
-	std::cout << "   (im) Calculate invariant mass of particle pair.\n";
+	std::cout << "   (im) Calculate invariant mass of particle pair using their energies, momenta and masses.\n";
 }
--- pp6calc.h
+++ pp6calc.h
@@ -2,19 +2,23 @@
 #define _PP6CALC_H_
 
 void input_error();
+std::vector<double> get_user_input(std::string);
+std::vector<double> interpret_input(std::string);
+
 double Add (double, double);
 double Subtract (double, double);
 double Multiply (double, double);
 double Divide (double, double);
 double Intercept (double, double);
 std::vector<double> Poly2Solve (double, double, double);
-double Vector3Length (double, double, double);
-double Vector4Length (double, double, double, double);
+double Size3Vec (double, double, double);
+double Size4Vec (double, double, double, double);
 double InvMass (double, double, double);
 
 void helptext();
 
-
+// Variable vector used by calculator
+std::vector<double> vars;
 
 
 
