6fd8dbc054fe7b758d1b20cb092f68defadab7b5
Martin Spangenberg <m.spangenberg@warwick.ac.uk>
2013-02-27 17:16:07 +0000

    Modularise menu system

--- pp6calculator.cpp
+++ pp6calculator.cpp
@@ -7,114 +7,69 @@
 #include "pp6lib/pp6math.hpp"
 #include "pp6lib/string_interpret.hpp"
 
-int main(/*int argc, char* argv[]*/)
+int main(int argc, char* argv[])
 {
 
-/*
-	if (argc == 2 && argv[1] == "-h") {
-		helptext();
-		return 0;
+	//Check if help argument has been typed
+	std::string arg;
+	if (argc > 1) {
+		for (int i=0; i<argc; ++i) {
+			arg = argv[i];
+			if (arg == "-h") {
+				helptext();
+				return 0;
+			}
+		}
 	}
-*/
 
-	std::string operation; // Variable to store operation chosen by user
 
-	//Define set of available operations
-	const int Noperations = 11;
-	std::string valid_operations[Noperations] = {"q","a","s","m","d","i","p","v3","v4","im","sort"};
-	std::set<std::string> valid_operations_set (valid_operations,valid_operations+Noperations); // Set of valid operations
+	// Display main menu
+	// This gives access to the submenus for each week of the course.
+	// It is placed in a while loop such that the program returns to
+	// the main menu when exiting a sumbenu.
 
-	while (true) // Loop over calculator menu until user quits with "q"
-	{
-		std::cout << "Choose operation:\n";
+	std::string operation;
+	bool showmenu = true;
+
+	while (true) {
+
+		if (showmenu) {
+			std::cout << "====================================\n";
+			std::cout << "pp6calculator main menu\n";
+			std::cout << "====================================\n";
+			std::cout << "1) Week 1\n";
+			std::cout << "2) Week 2\n";
+			std::cout << "3) Week 3\n";
+			std::cout << "q) Quit program\n";
+			std::cout << "------------------------------------\n";
+		}
+
+		showmenu = true;
+
+		std::cout << "Choose option: ";
 		std::cin >> operation; // Get operation from user
 
-		if (!valid_operations_set.count(operation)) { // Check if user input is valid
-			std::cout << "Error in operation choice!\n";
-			input_error();
-			continue;
-		} else if (operation == "q") { // Check if user wants to quit
-			return 0;
+		if (operation == "1") {
+			MenuWeek1();
 		}
 
-		vars = get_user_input(operation); // Prompt user for numbers to process depending on operation chosen
-		if (vars.size() == 0) { // If size is zero then there are errors in input
-			std::cout << "Error in input!\n";
-			continue;
+		else if (operation == "2") {
+			MenuWeek2();
 		}
 
-		// Calculate result depending on the operation chosen
-		if (operation == "a") {
-			std::cout << vars.at(0) << " + " << vars.at(1) << " = " << Add(vars.at(0),vars.at(1)) << std::endl;
-		}
-		
-		else if (operation == "s") {
-			std::cout << vars.at(0) << " - " << vars.at(1) << " = " << Subtract(vars.at(0),vars.at(1)) << std::endl;
-		}
-		
-		else if (operation == "m") {
-			std::cout << vars.at(0) << " * " << vars.at(1) << " = " << Multiply(vars.at(0),vars.at(1)) << std::endl;
-		}
-		
-		else if (operation == "d") {
-			if (vars.at(1) == 0) {
-				std::cout << "Division by zero!\n";
-				input_error();
-				continue;
-			}
-			std::cout << vars.at(0) << " / " << vars.at(1) << " = " << Divide(vars.at(0),vars.at(1)) << std::endl;
-		}
-		
-		else if (operation == "i") {
-			std::cout << "Line " << vars.at(0) << "*x+" << vars.at(1) << " intercepts x-axis at x = " << Intercept(vars.at(0),vars.at(1)) << std::endl;
-		}
-		
-		else if (operation == "p") {
-			std::vector<double> solutions;
-			Poly2Solve(vars.at(0),vars.at(1),vars.at(2),solutions);
-			std::cout << "Solution to 2nd degree polynomium " << vars.at(0) << "*x^2+" << vars.at(1) << "*x+" << vars.at(2) << "=0:\n";
-			for (unsigned int i=0; i<solutions.size(); ++i)
-			{
-				std::cout << "x" << i << " = " << solutions.at(i) << std::endl;
-			}
-			if (solutions.size() == 0) {
-				std::cout << "None" << std::endl;
-			}
-		}
-		
-		else if (operation == "v3") {
-			double result = Size3Vec(vars.at(0),vars.at(1),vars.at(2));
-			std::cout << "Size of 3-vector (" << vars.at(0) << "," << vars.at(1) << "," << vars.at(2) << ") = " << result << "\n";
-		}
-
-		else if (operation == "v4") {
-			double result = Size4Vec(vars.at(0),vars.at(1),vars.at(2),vars.at(3));
-			if (result >= 0) {
-				std::cout << "Size of 4-vector (" << vars.at(0) << "," << vars.at(1) << "," << vars.at(2) << "," << vars.at(3) << ") = " << result << "\n";
-			} else {
-				std::cout << "Invalid entry. Negative square of components\n";
-			}
-		}
-
-		else if (operation == "im") {
-			double result = InvMass(vars.at(0),vars.at(1),vars.at(2),vars.at(3),vars.at(4));
-			std::cout << "Invariant mass of particles M = " << result << std::endl;
-		}
-
-		else if (operation == "sort") {
-			BubbleSort(vars);
-			std::cout << "Sorted array: " << vars[0];
-			for (unsigned int i=1; i<vars.size(); ++i) {
-				std::cout << "," << vars[i];
-			}
-			std::cout << std::endl;
+		else if (operation == "3") {
+			MenuWeek3();
 		}
 
 		else if (operation == "q") {
 			break;
 		}
 
-		std::cout << "=================" << std::endl;
+		else {
+			std::cout << "No such option!" << std::endl;
+			std::cout << "-----------------" << std::endl;
+			showmenu = false;
+		}
 
 	}
 
@@ -123,11 +78,279 @@ int main(/*int argc, char* argv[]*/)
 }
 
 
+//==================================
+// Submenus
+//==================================
+
+void MenuWeek1()
+{
+
+	std::string operation;
+	std::vector<double> vars;
+	int outcode;
+
+	std::cout << "====================================\n";
+	std::cout << "Week 1\n";
+	std::cout << "====================================\n";
+	std::cout << "a) Addition\n";
+	std::cout << "s) Subtraction\n";
+	std::cout << "m) Multiplication\n";
+	std::cout << "d) Divison\n";
+	std::cout << "q) Quit to main menu\n";
+	std::cout << "------------------------------------\n";
+
+	while (true) {
+
+		vars.clear();
+
+		std::cout << "Choose option: ";
+		std::cin >> operation; // Get operation from user
+
+		//=====================================
+
+		if (operation == "a") {
+			std::cout << "Addition A+B. Enter as A,B:" << std::endl;
+			outcode = get_user_input(2,vars);
+			if (outcode != -1) {
+				std::cout << vars.at(0) << " + " << vars.at(1) << " = " << Add(vars.at(0),vars.at(1)) << std::endl;
+			}
+		}
+
+		//=====================================
+
+		else if (operation == "s") {
+			std::cout << "Subtraction A-B. Enter as A,B:" << std::endl;
+			outcode = get_user_input(2,vars);
+			if (outcode != -1) {
+				std::cout << vars.at(0) << " - " << vars.at(1) << " = " << Subtract(vars.at(0),vars.at(1)) << std::endl;
+			}
+		}
+
+		//=====================================
+
+		else if (operation == "m") {
+			std::cout << "Multiplication A*B. Enter as A,B:" << std::endl;
+			outcode = get_user_input(2,vars);
+			if (outcode != -1) {
+				std::cout << vars.at(0) << " * " << vars.at(1) << " = " << Multiply(vars.at(0),vars.at(1)) << std::endl;
+			}
+		}
+
+		//=====================================
+
+		else if (operation == "d") {
+			std::cout << "Division A/B. Enter as A,B:" << std::endl;
+			outcode = get_user_input(2,vars);
+			if (outcode != -1) {
+				if (vars.at(1) == 0) {
+					std::cout << "Division by zero!" << std::endl;
+					input_error();
+				} else {
+					std::cout << vars.at(0) << " / " << vars.at(1) << " = " << Divide(vars.at(0),vars.at(1)) << std::endl;
+				}
+			}
+		}
+
+		//=====================================
+
+		else if (operation == "q") {
+			break;
+		}
+
+		else {
+			std::cout << "No such option!" << std::endl;
+		}
+
+		if (outcode == -1) {
+			std::cout << "Error in input!" << std::endl;
+		}
+
+		std::cout << "-----------------" << std::endl;
+
+	}
+
+}
+
+
+//================================================================
+
+
+void MenuWeek2()
+{
+	std::string operation;
+	std::vector<double> vars;
+	int outcode;
+
+	std::cout << "====================================\n";
+	std::cout << "Week 2\n";
+	std::cout << "====================================\n";
+	std::cout << "i)  Line intercept with x-axis\n";
+	std::cout << "p)  Solve 2nd degree polynomium\n";
+	std::cout << "v3) Size of 3-vector\n";
+	std::cout << "v4) Size of 4-vector\n";
+	std::cout << "im) Invariant mass of particle pair\n";
+	std::cout << "q)  Quit to main menu\n";
+	std::cout << "------------------------------------\n";
+
+	while (true) {
+
+		vars.clear();
+
+		std::cout << "Choose option: ";
+		std::cin >> operation; // Get operation from user
+
+		//=====================================
+
+		if (operation == "i") {
+			std::cout << "Intercept of line A*x+B with x-axis. Enter as A,B:" << std::endl;
+			outcode = get_user_input(2,vars);
+			if (outcode != -1) {
+				std::cout << "Line " << vars.at(0) << "*x+" << vars.at(1) << " intercepts x-axis at x = " << Intercept(vars.at(0),vars.at(1)) << std::endl;
+			}
+		}
+
+		//=====================================
+
+		else if (operation == "p") {
+			std::cout << "Solution to 2nd order polynomial A*x^2+B*x+C=0. Enter as A,B,C:" << std::endl;
+			outcode = get_user_input(3,vars);
+			if (outcode != -1) {
+				std::vector<double> solutions;
+				Poly2Solve(vars.at(0),vars.at(1),vars.at(2),solutions);
+				std::cout << "Solution to 2nd degree polynomium " << vars.at(0) << "*x^2+" << vars.at(1) << "*x+" << vars.at(2) << "=0:" << std::endl;
+				for (unsigned int i=0; i<solutions.size(); ++i)
+				{
+					std::cout << "x" << i << " = " << solutions.at(i) << std::endl;
+				}
+				if (solutions.size() == 0) {
+					std::cout << "None" << std::endl;
+				}
+			}
+		}
+
+		//=====================================
+
+		else if (operation == "v3") {
+			std::cout << "Size of 3-vector (v1,v2,v3). Enter as v1,v2,v3:" << std::endl;
+			outcode = get_user_input(3,vars);
+			if (outcode != -1) {
+				double result = Size3Vec(vars.at(0),vars.at(1),vars.at(2));
+				std::cout << "Size of 3-vector (" << vars.at(0) << "," << vars.at(1) << "," << vars.at(2) << ") = " << result << std::endl;
+			}
+		}
+
+		//=====================================
+
+		else if (operation == "v4") {
+			std::cout << "Size of 4-vector (v0,v1,v2,v3). Enter as v0,v1,v2,v3:" << std::endl;
+			outcode = get_user_input(4,vars);
+			if (outcode != -1) {
+				double result = Size4Vec(vars.at(0),vars.at(1),vars.at(2),vars.at(3));
+				if (result >= 0) {
+					std::cout << "Size of 4-vector (" << vars.at(0) << "," << vars.at(1) << "," << vars.at(2) << "," << vars.at(3) << ") = " << result << std::endl;
+				} else {
+					std::cout << "Invalid entry. Negative sum of squares.\n";
+				}
+			}
+		}
+
+		//=====================================
+
+		else if (operation == "im") {
+			std::cout << "Invariant mass of particle pair. Enter as m1,m2,p1,p2,phi:" << std::endl;
+			outcode = get_user_input(5,vars);
+			if (outcode != -1) {
+				double result = InvMass(vars.at(0),vars.at(1),vars.at(2),vars.at(3),vars.at(4));
+				std::cout << "Invariant mass of particles M = " << result << std::endl;
+			}
+		}
+
+		//=====================================
+
+		else if (operation == "q") {
+			break;
+		}
+
+		else {
+			std::cout << "No such option!" << std::endl;
+		}
+
+		if (outcode == -1) {
+			std::cout << "Error in input!" << std::endl;
+		}
+
+		std::cout << "-----------------" << std::endl;
+
+	}
+
+}
+
+
+//================================================================
+
+
+void MenuWeek3()
+{
+
+	std::string operation;
+	std::vector<double> vars;
+	int outcode;
+
+	std::cout << "====================================\n";
+	std::cout << "Week 3\n";
+	std::cout << "====================================\n";
+	std::cout << "s) Sort numbers in descending order\n";
+	std::cout << "q) Quit to main menu\n";
+	std::cout << "------------------------------------\n";
+
+	while (true) {
+
+		vars.clear();
+
+		std::cout << "Choose option: ";
+		std::cin >> operation; // Get operation from user
+
+		//=====================================
+
+		if (operation == "s") {
+			std::cout << "Sort array a[N] of numbers in descending order. Input as a0,a1,a2,...:" << std::endl;
+			outcode = get_user_input(-1,vars);
+			if (outcode != -1) {
+				BubbleSort(vars);
+				std::cout << "Sorted array: " << vars[0];
+				for (unsigned int i=1; i<vars.size(); ++i) {
+					std::cout << "," << vars[i];
+				}
+				std::cout << std::endl;
+			}
+		}
+
+		//=====================================
+
+		else if (operation == "q") {
+			break;
+		}
+
+		else {
+			std::cout << "No such option!" << std::endl;
+		}
+
+		if (outcode == -1) {
+			std::cout << "Error in input!" << std::endl;
+		}
+
+		std::cout << "-----------------" << std::endl;
+
+	}
+
+}
+
+
 //=====================================
 // Other functions
 //=====================================
 
-// Function to call whenever an input exception is found
+// Function to call whenever an input error is found
 void input_error()
 {
 	std::cin.clear();
@@ -136,61 +359,24 @@ void input_error()
 
 
 // Function to get user input
-std::vector<double> get_user_input(std::string operation)
+int get_user_input(int nvars, std::vector<double>& variables)
 {
-	std::vector<double> variables;
-	std::string prompt_text;
-	int nvars;
-
-	if (operation == "a") {
-		prompt_text = "Addition A+B. Enter as A,B:";
-		nvars = 2;
-	} else if (operation == "s") {
-		prompt_text = "Subtraction A-B. Enter as A,B:";
-		nvars = 2;
-	} else if (operation == "m") {
-		prompt_text = "Multiplication A*B. Enter as A,B:";
-		nvars = 2;
-	} else if (operation == "d") {
-		prompt_text = "Division A/B. Enter as A,B:";
-		nvars = 2;
-	} else if (operation == "i") {
-		prompt_text = "Intersect of line A*x+B with x-axis. Enter as A,B:";
-		nvars = 2;
-	} else if (operation == "p") {
-		prompt_text = "Solution to 2nd order polynomial A*x^2+B*x+C=0. Enter as A,B,C:";
-		nvars = 3;
-	} else if (operation == "v3") {
-		prompt_text = "Size of 3-vector (v1,v2,v3). Enter as v1,v2,v3:";
-		nvars = 3;
-	} else if (operation == "v4") {
-		prompt_text = "Size of 4-vector (v0,v1,v2,v3). Enter as v0,v1,v2,v3:";
-		nvars = 4;
-	} else if (operation == "im") {
-		prompt_text = "Invariant mass of particle pair. Enter as m1,m2,p1,p2,phi:";
-		nvars = 5;
-	} else if (operation == "sort") {
-		prompt_text = "Sort array a[N] of numbers in descending order. Input as a0,a1,a2,...:";
-		nvars = -1; // Set to -1 since we don't know the size of the array to sort
-	}
-	
-	std::cout << prompt_text << std::endl;
-
 	std::string input;
 	std::cin >> input;
 
 	if (!std::cin) { // Check for errors in input
 		input_error();
-	}
-	else {
+		return -1;
+	}	else {
 		string_interpret(input,variables);
 	}
 
-	if (variables.size() != (unsigned)nvars && nvars != -1) {
+	if ( (variabl2es.size() != (unsigned)nvars && nvars != -1) || variables.size() == 0) {
 		variables.clear();
+		return -1;
+	}	else {
+		return 0;
 	}
-
-	return variables;
 
 }
 
@@ -205,15 +391,13 @@ void helptext()
 	std::cout << "========================\n";
 	std::cout << "How to use this program \n";
 	std::cout << "========================\n";
-	std::cout << "Enter the operation that you want to perform when prompted.\n";
-	std::cout << "   Available operations:\n";
-	std::cout << "   (a)  Addition a+b,\n";
-	std::cout << "   (s)  Subtraction a-b,\n";
-	std::cout << "   (m)  Multiplication a*b,\n";
-	std::cout << "   (d)  Division a/b,\n";
-	std::cout << "   (i)  Find intercept of straight line a*x+b with x-axis,\n";
-	std::cout << "   (p)  Solve 2nd degree polynomial a*x^2 + b*x + c = 0,\n";
-	std::cout << "   (v3) Calculate size of 3-vector (a, b, c),\n";
-	std::cout << "   (v4) Calculate size of 4-vector (a, b, c, d) assuming metric (1, -1, -1, -1),\n";
-	std::cout << "   (im) Calculate invariant mass of particle pair using their momenta, masses and their mutual angle.\n";
+	std::cout << "pp6calculator is a simple program for doing various kinds\n";
+	std::cout << "of calculations."; 
+	std::cout << "The program consists of a main menu which gives access to\n";
+	std::cout << "a number of submenus. Both the main menu and submenus are\n";
+	std::cout << "navigated by entering one or several symbols as described\n";
+	std::cout << "in the respective menu.\n";
+	std::cout << "The submenus contain options for mathematical operations\n";
+	std::cout << "on input given by the user. All input variables are entered\n";
+	std::cout << "in one line with each variable separated by a comma.\n";
 }
--- pp6calculator.hpp
+++ pp6calculator.hpp
@@ -2,11 +2,14 @@
 #define _PP6CALC_HPP_
 
 void input_error();
-std::vector<double> get_user_input(std::string operation);
+int get_user_input(int nvars, std::vector<double>& variables);
+
+void MenuWeek1();
+void MenuWeek2();
+void MenuWeek3();
+
 void helptext();
 
-// Variable vector used by calculator
-std::vector<double> vars;
 
 
 
